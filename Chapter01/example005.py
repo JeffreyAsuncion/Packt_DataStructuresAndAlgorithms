56cd78283f316fc3de6e8250996611f7053e065f
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-09-28 06:33:34 -0400

    Overview of data types and objects

---
+++ Chapter01/example005.py
@@ -0,0 +1,12 @@
+# Data types and Objects
+
+a = 1; b = 1
+
+# a and b have the same value
+if a == b: print("We have the same value")
+
+# if a and b are the same object
+if a is b: print("We are the same Object")
+
+# if a and b are the same type
+if type(a) is type(b) : print("We are the same Type")
