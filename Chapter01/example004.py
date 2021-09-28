c8be5677433ef6021849b7cb421cd579e7d5bac1
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-09-27 21:00:22 -0400

    flow control and iteration

---
+++ Chapter01/example004.py
@@ -0,0 +1,13 @@
+# Flow control and iteration
+
+x = 0
+while x < 3:
+    print(x); x += 1
+
+x = 'one'
+if x == 0:
+    print('False')
+elif x == 1:
+    print('True')
+else:
+    print('Something else')
