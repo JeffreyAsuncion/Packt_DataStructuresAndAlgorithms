e30ce8f6b35fe4acdd479f9db8bfb43b5f46184b
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-02 16:39:35 -0400

    Classes and Object Programming

--- Chapter01/example013.py
+++ Chapter01/example013.py
@@ -12,9 +12,19 @@ class Employee(object):
         Employee.numEmployee -= 1
 
     def hours(self, numHours):
-        self.owed + numHours * self.rate
+        self.owed = numHours * self.rate
         return("%.2f hours worked" % numHours)
 
     def pay(self):
         self.owed = 0
         return("paid %s " % self.name)
+
+emp1 = Employee("Jill", 18.50)
+emp2 = Employee("Jack", 15.50)
+
+print(Employee.numEmployee)
+
+print(emp1.hours(20))
+print(emp1.owed)
+print(emp1.pay())
+
