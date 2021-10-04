60997950d455e6d8da4e9165d80a9cb9be69c6e8
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-04 06:47:40 -0400

    Numeric types

---
+++ Chapter02/example001.py
@@ -0,0 +1,11 @@
+# Numeric Types
+a=4; b=5    #Operator (=) assigns the value to variable
+print(a, "is of type", type(a))
+
+print(9/5)
+
+c = b/c     # division returns a floating point number
+print(c, "is of type", type(c))
+
+# print(c)    # No need to explicitly declare the datatype
+
