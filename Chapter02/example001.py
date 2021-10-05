9382c3cea030e212a2b769172fa765262eaebc00
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-05 10:21:01 -0400

    Numeric Types 1

--- Chapter02/example001.py
+++ Chapter02/example001.py
@@ -4,8 +4,17 @@ print(a, "is of type", type(a))
 
 print(9/5)
 
-c = b/c     # division returns a floating point number
+c = b/a     # division returns a floating point number
 print(c, "is of type", type(c))
 
-# print(c)    # No need to explicitly declare the datatype
+print(c)    # No need to explicitly declare the datatype
 
+
+
+a=4; b=5
+d = b // a
+print(d, "is of type", type(d))
+
+print('true division',7/5) # true division
+
+print('floor division operator',7//5) # floor division operator
