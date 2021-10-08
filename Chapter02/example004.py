d89c2fc49c532aec1e2f3f42c4a213eb841f0aff
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-08 07:09:32 -0400

    Membership, Identity, and logical operators

---
+++ Chapter02/example004.py
@@ -0,0 +1,11 @@
+# Membership, identity, and logical operations
+
+x=[1,2,3]
+y=[1,2,3]
+
+print(x==y)  #test equivalance
+
+print(x is y) #test object identity
+
+x=y  # assignment   
+print(x is y) 
