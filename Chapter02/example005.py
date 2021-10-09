a149a6ab2f6b0b05a797a7d102ade15cfa49ee28
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-09 18:20:56 -0400

    Sequences

---
+++ Chapter02/example005.py
@@ -0,0 +1,16 @@
+# Sequences
+
+print("this is an empty list: ", list())   
+
+list1 = [1,2,3,4]
+print("before : ",list1)
+list1.append(1)
+print("after  : ",list1)
+
+list2 = list1 * 2
+print(list2)
+
+print("min : ", min(list1))
+
+
+print("max : ", max(list1))
