0c72ad901e5a448e148c932fefe86ba053dab288
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-13 05:29:45 -0400

    Dictionaries part2

--- Chapter02/example010.py
+++ Chapter02/example010.py
@@ -4,3 +4,26 @@ print(dict(zip('packt', range(5))))
 
 a = dict(zip('packt', range(5)))
 print(len(a))
+
+print(a['c'])   # to check the value of a key
+
+print(a.pop('a'))
+
+print(a)
+
+b = a.copy()    # make a copy of the dictionary
+
+print(b)
+
+print(a.keys())
+
+print(a.values())
+
+print(a.items())
+
+a.update({'a':1})   # add an item in the dictionary
+print(a)
+
+a.update(a=22)  # update the value of key 'a'
+
+print(a)
