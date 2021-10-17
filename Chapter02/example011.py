b1b1cb65e819d7965885742f85e12727bb3474ba
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-17 07:06:51 -0400

    Sorting Dictionaries

--- Chapter02/example011.py
+++ Chapter02/example011.py
@@ -17,3 +17,45 @@ list_comprehension = [value for (key,value) in sorted(d.items())]
 print(list_comprehension)
 
 print(sorted(list(d), key=d.__getitem__, reverse=True))
+
+d2 = {
+          'one':'isa',
+          'two':'dalawa',
+          'three':'tatlo',
+          'four':'apat',
+          'five':'lima',
+          'six':'six'
+}
+print('d2')
+print(sorted(d2, key=d.__getitem__))
+
+print('d2 list comprehension')
+
+list_comprehension_d2 = [d2[i] for i in sorted(d2, key=d.__getitem__)]
+print(list_comprehension_d2)
+
+def corder(string):
+     return (string[len(string)-1])
+
+
+print('sort by last letter')
+print(sorted(d2.values(), key=corder))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
