6d0407e9f603778be9e6da008c14b2a0225d02a2
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-10-17 07:26:36 -0400

    Counting Dictionary

---
+++ Chapter02/example012.py
@@ -0,0 +1,25 @@
+# Dictionaries for text analysis
+
+def wordcount(fname):
+    try:
+        fhand=open(fname)
+    except:
+        print('File can not be opened')
+        exit()
+
+    count=dict()
+    for line in fhand:
+        words=line.split()
+        for word in words:
+            if word not in count:
+                count[word]=1
+            else:
+                count[word]=+1
+    return(count)
+
+count=wordcount('alice.txt')
+
+filtered={key:value for key, value in count.items() if value < 20 and value>16}
+
+print(count)
+print(filtered)
